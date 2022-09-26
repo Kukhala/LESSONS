226509a8cd8c1b96a60e94bc9d16c1151e84b2ba
Kukhala <sandrokukhaleishvili@gmail.com>
2022-09-26 20:06:46 +0400

    Gakvetili 1

--- GROUP 102/დავალება1.py
+++
@@ -1,8 +0,0 @@
-name = "My Name is SANDRO"
-surname = "KUKHALEISHVILI"
-age1 = "My Age is"
-height1 = "My Height is"
-age = 26
-height = 81.3
-
-print (name + " " + surname + " " + age1 + " " + str(age) + " " + height1 + " " + str(height))
