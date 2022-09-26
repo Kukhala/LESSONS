6541288a8075e44746d79dd2b216c889b34f7608
Kukhala <sandrokukhaleishvili@gmail.com>
2022-09-26 20:13:50 +0400

    HM 1

---
+++ GROUP 102/დავალება1.py
@@ -0,0 +1,19 @@
+# name = "My Name is SANDRO"
+# surname = "KUKHALEISHVILI"
+# age1 = "My Age is"
+# height1 = "My Height is"
+# age = 26
+# height = 81.3
+
+# print (name + " " + surname + " " + age1 + " " + str(age) + " " + height1 + " " + str(height))
+
+
+name = "sandro"
+surname = "kukhaleisvili"
+age = "26"
+height = "90.3"
+
+print (name + " " + surname + " " + str(age) + " " + str(height))
+
+
+print ("""my namge is sandro kukhaleishvili 26 old""")
