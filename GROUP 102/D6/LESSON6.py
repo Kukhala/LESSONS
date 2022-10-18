83faf86dc8fe0d6668e44f1cdc6926cf808088e6
Kukhala <sandrokukhaleishvili@gmail.com>
2022-10-18 19:56:28 +0400

    Lesson 6

---
+++ GROUP 102/D6/LESSON6.py
@@ -0,0 +1,58 @@
+# x = 0
+# for i in range (5):
+#     for j in range (4):
+#         for h in range (3):
+#              print (str(x) + " Sandro")
+#              x += 1
+
+
+# შემოგვაქ ასაკი, ვინც 3-ზე მეტისააა იხდის ფულს
+
+# 18
+# 24
+# 2
+# 5
+# 42
+
+# 4 * 100$ = 400 $
+
+# total_price = 0
+# for i in range(5):
+#    age_of_user = int (input("Enter Users Age: "))
+#    if age_of_user >= 3:
+#     total_price += 100
+
+# print (total_price)
+
+# while - ით
+
+# total_price = 0
+# i = 0
+# while i < 4:
+#     age_of_user = int(input("Enter Users Age: "))
+#     if age_of_user >= 3:
+#         total_price += 100
+#         i += 1
+
+# print (total_price)
+
+
+# list - სია
+# int, float, booLean, list
+
+my_list = ["xinkali", "mwvadi", "lobiani", "qababi", "kachapuri", "wyali"]
+# სია იქქმნება კვადრატული ფრჩხილით და ელემენტები გამოყოფილია ფრჩხილით
+prices = [2,20,15,10,15,2]
+
+weird_list = ["sandro", "sandro2", "sandro3", [1,2,3], "sandro4", [4,5,6]]
+
+# print (my_list[1])
+# გამოსატანად ვირჩევთ რიგის ნომერს
+
+# print (weird_list[5][2])
+# შიგნით მყოფის გამოსახვისთვის ჯერ ვწერთ რომელია ინდექასად მთლიანი და შემდეგ კონკრეტული შიგნით
+
+for food in my_list:
+    print(food)
+
+
