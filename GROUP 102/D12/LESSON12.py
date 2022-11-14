4ae7df939034335e3ffce94b2676a48330d453ab
Kukhala <sandrokukhaleishvili@gmail.com>
2022-11-14 23:01:30 +0400

    Lesson 12:

---
+++ GROUP 102/D12/LESSON12.py
@@ -0,0 +1,29 @@
+# arr = ["nika", "luka", "giorgi", "iva"]
+
+# # i = 0
+# # for element in arr:
+# #     print(i,element)
+
+# #     i =+ 1
+
+# # or
+
+# # for i in range(1, len(arr) +1):
+# #     print(i, arr[i-1])  
+
+# # or
+
+# for count, value in enumerate(arr):
+#     print(count,value)
+
+# numerate - გადაეცემა ორი ცვლადი და ქმნის ნუმერაციას, აუცილებელია ორი საიტერაციო ცვლადი
+
+
+my_str = "mamika kargi bikia"
+
+print(my_str.split())
+
+# split - მისი წყალობით შეგვიძლია შევქმნათ სია
+
+print(my_str.split("k"))
+
