e68d52915688a02329e870ff0485e9a1674af174
Kukhala <sandrokukhaleishvili@gmail.com>
2022-11-14 22:54:20 +0400

    Lesson 9

---
+++ GROUP 102/D9/LESSON9.py
@@ -0,0 +1,29 @@
+# def reverse_arr(any_arr):
+
+#     new_arr = []
+
+#     i = len(any_arr)
+
+#     while i > 0:
+#         new_arr.append(any_arr[i-1])
+#         i -= 1
+
+#     print(new_arr)
+
+
+# შექმენით ფუნცია რომელსაც გადაეცემა ორი მოთამაშის ქულა
+# დაპრინტოს წინადადება " ამას უფრო მეტი ქულა აქვს ამდენით"
+
+# def compare_score(p1_score, p2_score):
+
+#     if p1_score > p2_score:
+#         print ("{} metia {}-ze, {}-it".format(p1_score, p2_score, p1_score-p2_score))
+#     elif p2_score > p1_score:
+#         print ("{} metia {}-ze, {}-it".format(p2_score, p1_score, p2_score-p1_score))
+
+# compare_score(22,43)
+
+def multiply(a, b):
+    return a * b
+
+# print(multiply(10,20))
---
+++ GROUP 102/D9/TEST.py
@@ -0,0 +1,21 @@
+# ჩვენი შექმნილი ფუნქცია შეგვიძლია გამოვიძახოთ და მარტივად გამოვიყენოთ
+
+from LESSON9 import reverse_arr
+# from LESSON9 import multiply
+# from LESSON9 import compare_score
+
+students = ["nika", "ana", "mamuka", "beqa", "dachi", "iva",]
+# reverse_arr(students)
+
+# students2 = ["sandro", "eva", "gigi", "nica"]
+# reverse_arr(students2)
+
+# from LESSON9 import compare_score
+
+# compare_score(24,23)
+
+
+# from LESSON9 import multiply
+
+# print(multiply (10,23))
+
